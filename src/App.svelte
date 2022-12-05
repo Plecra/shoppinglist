5ea0137916e366b2b2ee1584b2006a90f7a31b9d
Marli Frost <marli@frost.red>
2022-12-05 14:50:35 +0000

    blunt approach to syncing list with firebase

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
+  import { onValue, set, type DatabaseReference } from "firebase/database";
+  import { onDestroy } from "svelte";
   import Task from "./lib/Task.svelte";
-
+  export let dbRef: DatabaseReference;
   function randid() {
     return Math.floor(Math.random() * 10000)
   }
-  let tasks = [{ title: "One", id: randid(), selected: false }];
+  let have_synced = false;
+  let tasks = [{ title: "", id: randid(), selected: false }];
+  onDestroy(onValue(dbRef, function(v) {
+    tasks = v.val().map(({ title, id }) => ({ title, id, selected: false }));
+    have_synced = true;
+  }))
+  $: {
+    if (have_synced)
+      set(dbRef, tasks.map(({ title, id }) => ({ title, id })))
+  }
   let me;
 </script>
 
 <main bind:this={me}>
+  <!-- {#each tasks as { title, selected, id }, i (id)} -->
   {#each tasks as { title, selected, id }, i (id)}
     <Task bind:title bind:selected
       on:gotonext={() => {
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import './app.css'
 import App from './App.svelte'
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, push, set } from "firebase/database";
+
+const firebaseApp = initializeApp({
+  databaseURL: "https://shoppinglist-31849-default-rtdb.europe-west1.firebasedatabase.app/",
+});
+const database = getDatabase(firebaseApp);
+const items = ref(database, "items");
+// const new_item = push(items);
+// set(new_item, "Apples");
+
 const app = document.createElement('div');
-new App({ target: app });
+new App({ target: app, props: { dbRef: items } });
 
 if (document.readyState === "loading") document.addEventListener("readystatechange", ready, { once: true })
 else ready.call(document, null)
