1ffd83fce0d9d793e70d741fe56ea66f57027157
Marli Frost <marli@frost.red>
2022-12-05 15:07:54 +0000

    Configure Vite to use relative paths
    This fixes the page when used at a path below the top level
    (domain.com/listpage/... works as well as domain.com/.,..)

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), basicSsl()]
+  plugins: [svelte(), basicSsl()],
+  base: "./"
 })
